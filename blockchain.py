070bba55562f1e8c9b0ec676a9b5158ee89839d0
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2020-05-16 15:34:58 -0400

    blockchain files
    block and blockchain structure

---
+++ blockchain.py
@@ -0,0 +1,15 @@
+from block import Block
+
+blockChain = [Block.create_gen_block()]
+
+print("The genesis block has been constructed")
+print("Hash: %s" % blockChain[-1].hash)
+
+amountOfAppendedBlocks = 10
+
+for i in range(1, amountOfAppendedBlocks + 1):
+    blockAppend(Block(blockChain[-1].hash,
+                        "DATA", datetime.datetime.now()))
+
+    print("Block #%d has been created." % i)
+    print("Block #%d hash: %s" % (i, blockChain[i].hash))
