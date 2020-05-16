cb56318849955e0fb869cca3eeeeddf3c5b0eb58
nikhilmsreedhar <65008600+nikhilmsreedhar@users.noreply.github.com>
2020-05-16 16:24:13 -0400

    update
    update

---
+++ block.py
@@ -0,0 +1,22 @@
+import hashlib
+import datetime
+
+class Block:
+    def __init__(current, prevHash, data, timeStamp):
+        current.prevHash = prevHash
+        current.data = data
+        current.timeStamp = timeStamp
+        current.hash = current.getHash()
+
+    @staticmethod
+    def create_gen_block():
+        return Block("0", "0", datetime.datetime.now())
+
+    def getHash(current):
+        header_bin = (str(current.prevHash) +
+            str(current.data) +
+            str(current.timeStamp)).encode()
+
+        innerHash = hashlib.sha256(header_bin).hexdigest().encode()
+        outerHash = hashlib.sha256(innerHash).hexdigest()
+        return outerHash
